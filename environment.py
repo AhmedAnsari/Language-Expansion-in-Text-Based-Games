8ff6c0a16680e71555e5448d2fdc0fafaef8618b
jpsagarm95 <jpsagarm95@gmail.com>
2016-09-29 20:53:53 +0530

    Adds sockets to environment.py.

--- environment.py
+++ environment.py
@@ -5,6 +5,7 @@ Created on Sat Aug 20 15:05:34 2016
 @author: ghulamahmedansari
 """
 
+import zmq
 import numpy as np
 from collections import deque
 import zmq
@@ -12,6 +13,9 @@ import sys
 
 class Environment(object):
     def __init__(self, config):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect ("tcp://localhost:12345")
         self.START_NEW_GAME = True
         self._env = gym.make(config.GAME)
         self.CURR_REWARD = 0 
@@ -23,14 +27,43 @@ class Environment(object):
         # Define the socket using the "Context"
         sock = context.socket(zmq.REQ)
 
-        
+    
+    def getScrRewTer(self, msg):
+        msgsplit = msg.split("#")
+        self._screen_ = [float(i) for i in msgsplit[0].split(" ")]
+        self.reward = float(msgsplit[1])
+        self.terminal = float(msgsplit[2])
+        return self._screen_, self.reward, self.terminal
+
+    def interact(self, str):
+        self.socket.send(str)
+        msg = self.socket.recv()
+        return msg
+
+
+    def newGame(self):
+        str = 'newGame'
+        msg = self.interact(str)
+        self._screen_, self.reward, self.terminal = getScrRewTer(msg)
+        return self._screen_, self.reward, self.terminal
+
     def step(self,action):
-        socket.send('step_game')
-        self._screen_, self.reward, self.terminal, _ = 
+        str = 'step_game'
+        msg = self.interact(str)
+        self._screen_, self.reward, self.terminal = getScrRewTer(msg)
+        return self._screen_, self.reward, self.terminal
+        # self._screen_, self.reward, self.terminal, _ = self._env.step(action)         
                
     def action_size(self):
-        return self._env.action_space.n        
+        str = 'getActions'
+        msg = self.interact(str)
+        return int(msg)
             
+    def object_size(self):
+        str = 'getObjects'
+        msg = self.interact(str)
+        return int(msg)
+
     def act(self,action):
         Reward = 0
         start_lives = self._env.ale.lives()
