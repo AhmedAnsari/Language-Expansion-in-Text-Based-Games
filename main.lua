ad9b4483ad6d7ff69e4b851c732c3360c1b16876
Ghulam Ahmed Ansari <ghulamahmedansari@live.in>
2016-10-16 17:14:00 +0530

    simplified game starting and closing

--- main.lua
+++ main.lua
@@ -9,9 +9,17 @@ print(port)
 client_connect(port)
 login('root', 'root')
 counter = arg[1]
-framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims/build.ev")
-framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims3/build.ev")
-framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims4/build.ev")
+if arg[1]==1 then
+    framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims/build.ev")
+elseif arg[1]==2 then
+    framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims3/build.ev")
+elseif arg[1]==3 then
+    framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims4/build.ev")
+elseif arg[1]==4 then
+    framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims/build.ev")
+    framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims3/build.ev")
+    framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims4/build.ev")
+end
 framework.writeSymbolMapping(arg[1])
 print("#symbols", #symbols)
 framework.interact(counter)
