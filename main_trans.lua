9a22d0911cf4db61d49cd1e973a17a1bcd1e70d0
Ghulam Ahmed Ansari <ghulamahmedansari@live.in>
2016-10-29 14:57:30 +0530

    minor fix

--- main_trans.lua
+++ main_trans.lua
@@ -5,7 +5,7 @@ require 'utils'
 
 
 
-if arg[1]!='6' then
+if arg[1] ~= '6' then
     local port = 4000 + tonumber(arg[1])
     print(port)
     client_connect(port)
@@ -18,7 +18,7 @@ if arg[1]!='6' then
     framework.writeSymbolMapping("1236")
 	print("#symbols", #symbols)
 	framework.interact(counter)    
-elseif arg[1]=='6' then
+elseif arg[1] =='6' then
     framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims/build.ev")
     framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims3/build.ev")
     framework.makeSymbolMapping("../text-world/evennia/contrib/text_sims4/build.ev")
