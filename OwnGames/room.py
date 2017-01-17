0087d6843358fa7853e87b135cb38268d6c3e69d
jpsagarm95 <jpsagarm95@gmail.com>
2017-01-17 16:39:38 +0530

    Creates OwnGames.

---
+++ OwnGames/room.py
@@ -0,0 +1,8 @@
+class Room:
+	def __init__(self, num, name, desc, neighbors, obj, act):
+		self.num = num
+		self.name = name
+		self.desc = desc
+		self.neighbors = neighbors
+		self.object = obj
+		self.action = act
